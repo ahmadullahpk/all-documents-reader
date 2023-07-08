363ef849e472f0ae57b104ac07a0bb90f3d690c5
Ahmad Ullah <sbkahmadullah@gmail.com>
2023-07-08 23:19:56 +0500

    up

--- pdfviewer/src/main/java/com/artifex/mupdf/fitz/DisplayListDevice.java
+++
@@ -1,9 +0,0 @@
-package com.artifex.mupdf.fitz;
-
-public final class DisplayListDevice extends NativeDevice {
-   private static native long newNative(DisplayList var0);
-
-   public DisplayListDevice(DisplayList list) {
-      super(newNative(list));
-   }
-}
